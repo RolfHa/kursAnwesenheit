7fc4dae967597591b879268322a614412229e244
RolfHaeckel <rolf.haeckle@mac.com>
2024-05-22 14:55:26 +0200

    Projekt initialisiert, workaround: erst Teilnehmer, dann Dozenten generiert um user_ids von 1 bis 30 als Teilnehmer zu erhalten

---
+++ db/db.sql
@@ -0,0 +1,49 @@
+drop database if exists anwesenheit;
+
+create database anwesenheit;
+
+use anwesenheit;
+
+create table user
+(
+    id     int primary key auto_increment,
+    fname  varchar(255),
+    lname  varchar(255),
+    email  varchar(255),
+    pwhash varchar(255),
+    role   varchar(255)
+);
+
+create table teilnehmer
+(
+    id           int primary key auto_increment,
+    user_id      int,
+    fachrichtung varchar(255),
+    team         varchar(255),
+    foreign key (user_id) references user (id)
+
+);
+
+create table anwesenheit
+(
+    id            int primary key auto_increment,
+    dozenten_id   int,
+    teilnehmer_id int,
+    datum         date,
+    status        varchar(255),
+    foreign key (dozenten_id) references user (id),
+    foreign key (teilnehmer_id) references teilnehmer (id)
+);
+
+# SELECT u.*,
+#        t.fachrichtung,
+#        t.team,
+#        t.user_id,
+#        t.id
+# FROM user u  left join teilnehmer t on u.id = t.user_id
+# where team is not null ;
+#
+#
+# SELECT count(id)  FROM user where role='teilnehmer';
+#
+# SELECT * FROM anwesenheit where teilnehmer_id=6 and month(datum) = 1;
---
+++ view/teilnehemer/show.php
@@ -0,0 +1,69 @@
+<?php
+include '../../class/User.php';
+include '../../class/Teilnehmer.php';
+include '../../class/Anwesenheit.php';
+
+$teilnehmer_id = $_GET['id'];
+$anwesenheiten = Anwesenheit::findByMonthTId($teilnehmer_id, 1);
+$teilnehmer = Teilnehmer::findById($teilnehmer_id);
+?>
+<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport'
+          content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>Document</title>
+</head>
+<body>
+
+<?php echo '<h1>' . $teilnehmer->getFname() . " " . $teilnehmer->getLname() . '</h1>'; ?>
+<div>
+    <?php
+    $color = 'withe';
+
+
+    echo '<div style="display: flex">';
+    foreach ($anwesenheiten as $index => $item) {
+        if ($item->getStatus() == 'fe') {
+            $color = 'green';
+            $text = 'Frei';
+        } elseif ($item->getStatus() == 'x') {
+            $color = 'dodgerblue';
+            $text = 'Anwesend';
+        } elseif ($item->getStatus() == 'o') {
+            $color = 'darkslateblue';
+            $text = 'Online';
+        } elseif ($item->getStatus() == 'n') {
+            $color = 'red';
+            $text = 'Nicht Dort';
+        }
+
+        if ($index == 0) {
+            $zeile = 0;
+            if ($item->getDatumObj()->format('w' == 0) or $item->getDatumObj()->format('w') == 6) {
+                $leer = 0;
+            } else {
+                $anzahl = $item->getDatumObj()->format('w');
+                $leer = $anzahl - 1;
+            }
+            $day = $item->getDatumObj()->format('w');
+            for ($i = 0; $i < $leer; $i++) {
+                echo "<span style='border-style: solid;align-self: stretch; align-self: stretch; width: 100px;display: inline-block; background-color: gray'>-</span>";
+
+                $zeile++;
+            }
+        }
+        echo "<span style='border-style: solid;width: 100px;display: inline-block; background-color: $color'>";
+        echo $item->getDatum();
+        echo "<br>";
+        echo $text;
+        echo '</span>';
+        $zeile++;
+        ($zeile % 7 == 0) ? printf(" </div><div>") : printf("");
+    }
+    ?>
+</div>
+</body>
+</html>
